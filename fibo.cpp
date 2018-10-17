711929d33e0ba4acfaf53d84e3c47caa34653bb5
Dimos Katsimardos <dimoskatsimardos@protonmail.com>
2018-10-17 18:00:55 +0300

    Fibonacci series vector STL

--- fibo.cpp
+++ fibo.cpp
@@ -3,32 +3,38 @@
 
 using namespace std;
 
-vector<int> fibo_nums;
-
-void find_fibo(int limit) {
-  /* ------------- */
-  int sum = 0;
-  for(int i=0; i<limit; i++)
-    if( i==0 || i==1 )
-      fibo_nums.push_back(i);
-    else
-      fibo_nums.push_back( fibo_nums[i-1] + fibo_nums[i-2] );
-}
-
-void print() {
-  vector<int>::iterator itr = fibo_nums.begin();
-  while(itr!=fibo_nums.end()) {
-    cout << *itr << " ";
-    itr++;
-  }
-  cout << "\n";
-}
+class FIBO {
+private:
+  vector<int> fibo_nums;
+  int limit;
+public:
+  FIBO(int limit_) : limit(limit_) {}
+  ~FIBO() {}
+  void find_fibo() {
+    /* ------------- */
+    int sum = 0;
+    for(int i=0; i<limit; i++)
+      if( i==0 || i==1 )
+        fibo_nums.push_back(i);
+      else
+        fibo_nums.push_back( fibo_nums[i-1] + fibo_nums[i-2] );
+    }
+    void print() {
+      vector<int>::iterator itr = fibo_nums.begin();
+      while(itr!=fibo_nums.end()) {
+        cout << *itr << " ";
+        itr++;
+      }
+      cout << "\n";
+    }
+  };
 
 
 main() {
 
-  find_fibo(21);
-  print();
+  FIBO Obj(21);
+  Obj.find_fibo();
+  Obj.print();
 
   return 0;
 }
