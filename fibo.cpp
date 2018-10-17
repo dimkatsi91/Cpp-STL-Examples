4f39b29a59d382d6e35b74af85f84fc7898e0502
Dimos Katsimardos <dimoskatsimardos@protonmail.com>
2018-10-17 17:55:21 +0300

    Fibonacci series vector STL

--- fibo.cpp
+++ fibo.cpp
@@ -8,14 +8,11 @@ vector<int> fibo_nums;
 void find_fibo(int limit) {
   /* ------------- */
   int sum = 0;
-  vector<int>::iterator itr = fibo_nums.begin();
   for(int i=0; i<limit; i++)
     if( i==0 || i==1 )
       fibo_nums.push_back(i);
-    else {
+    else
       fibo_nums.push_back( fibo_nums[i-1] + fibo_nums[i-2] );
-      itr++;
-    }
 }
 
 void print() {
