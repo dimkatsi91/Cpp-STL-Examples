7aa40e453779050ef3113f2482f18e4d931b7ee3
Dimos Katsimardos <dimoskatsimardos@protonmail.com>
2020-02-14 00:32:20 +0200

    Update testHeader.cpp

--- FirstGoogleTest/testHeader.cpp
+++ FirstGoogleTest/testHeader.cpp
@@ -34,3 +34,36 @@ int main(int argc, char** argv) {
 	testing::InitGoogleTest(&argc, argv);
 	return RUN_ALL_TESTS();
 }
+
+/* KONSOLE OUTPUT
+
+[==========] Running 4 tests from 2 test cases.
+[----------] Global test environment set-up.
+[----------] 3 tests from FactTest
+[ RUN      ] FactTest.smallNumber
+[       OK ] FactTest.smallNumber (0 ms)
+[ RUN      ] FactTest.NegativeOrZero
+[       OK ] FactTest.NegativeOrZero (0 ms)
+[ RUN      ] FactTest.BigNumber
+/home/dimkatsi91/MyDocuments/Cpp-STL-Examples/FirstGoogleTest/testHeader.cpp:20: Failure
+      Expected: 2432902008176640000
+To be equal to: fact(20)
+      Which is: -2102132736
+[  FAILED  ] FactTest.BigNumber (0 ms)
+[----------] 3 tests from FactTest (0 ms total)
+
+[----------] 1 test from FiboTest
+[ RUN      ] FiboTest.findFiboNums
+[       OK ] FiboTest.findFiboNums (0 ms)
+[----------] 1 test from FiboTest (0 ms total)
+
+[----------] Global test environment tear-down
+[==========] 4 tests from 2 test cases ran. (1 ms total)
+[  PASSED  ] 3 tests.
+[  FAILED  ] 1 test, listed below:
+[  FAILED  ] FactTest.BigNumber
+
+ 1 FAILED TEST
+
+
+*/
