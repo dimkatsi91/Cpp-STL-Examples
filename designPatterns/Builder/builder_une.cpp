24b3f75893193887f161ecfd94887788e750748c
Dimos Katsimardos <dimoskatsimardos@protonmail.com>
2020-02-15 18:27:45 +0200

    exploring builder | simple example

---
+++ designPatterns/Builder/builder_une.cpp
@@ -0,0 +1,95 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+enum class Position {
+    GK,
+    Defender,
+    Midfielder,
+    Striker
+};
+
+// class for a player
+class Player {
+private:
+    string name;
+    Position position;
+public:
+    Player() {}
+    Player(const Position & position, const string & name)
+        : name(name), position(position) {}
+    string getPos() const {
+        switch(position) {
+            case Position::GK:
+                return "GoalKeeper";
+                break;
+            case Position::Defender:
+                return "Defender";
+                break;
+            case Position::Midfielder:
+                return "MidFielder";
+                break;
+            default:
+                return "Striker";
+                break;
+        }
+    }
+    void info() {
+        cout << "Player " << name;
+    }
+    void setName(const string & name) { this->name=name; }
+    void setPosition(const Position & position) { this->position=position; }
+};
+
+
+class PlayerBuilder {
+    Player player;
+public:
+    PlayerBuilder(const string & name,const  Position & position) {
+        player.setName(name);
+        player.setPosition(position);
+    }
+    void builderInfo() {
+        player.info();
+        cout <<  " is a " << player.getPos() << endl;
+    }
+};
+
+// let's create a team of 5 players only | It is goingto play in 5X5
+//
+class FootballTeam {
+private:
+    vector<PlayerBuilder> players;
+    int squadSize;
+    string name;
+public:
+    FootballTeam(vector<PlayerBuilder> players, int squadlen, const string& teamName) 
+        : players(players), squadSize(squadlen), name(teamName) 
+    {
+        for(auto &player : players) {
+            cout << " Adding a plyer in team: " << name << "\n";
+            player.builderInfo();
+        }
+    }
+};
+
+
+int main() {
+    
+    vector<PlayerBuilder> players{ {"Nikos", Position::GK}, 
+                                   {"Thom", Position::Defender}, 
+                                   {"Dimitri", Position::Midfielder},
+                                   {"George", Position::Midfielder},
+                                   {"Dimos", Position::Striker} };
+
+    FootballTeam arakynthos
+        {
+            players,
+            5,
+            "Arakynthos United"
+        };
+
+    return 0;
+}
