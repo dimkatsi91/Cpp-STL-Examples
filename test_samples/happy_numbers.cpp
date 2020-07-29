3622025e602b94b57477e0dcd4c75ada6dfc28ee
Dimost Katsimardos <dimoskatsimardos@protonmail.com>
2020-07-29 18:52:04 +0300

    happy numbers problem | simple solution presented 1st..

---
+++ test_samples/happy_numbers.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+// This function returns a vector of integers
+// integers are the digits of the num
+// For example :: 
+// Input: 13
+// Output:  vector<int> digits{1,3}
+//
+vector<int> digits_of_num(int num)
+{
+    vector<int> digits{};
+    while(num!=0)
+    {
+        digits.push_back(num%10);
+        num /= 10;
+    }
+    return digits;
+}
+
+// Just calculate the sum of sdquares of integers of a vector
+//
+int calc_squares_sum(vector<int> nums)
+{
+    int sum=0;
+    for(auto & num : nums)
+        sum += num*num;
+    return sum;
+}
+
+
+// This function checks if an integer number is a happy number or not
+// returns a string "happy" or "sad"
+// Happy number is a number that eventually reaches '1' when replaced
+// by the sum of the square of each digit
+//
+string is_happy(int num)
+{
+    int tmp = num;
+    int counter=0;
+
+    if(num==1)
+        return "happy";
+    if(num<=0)
+        return "sad";
+    
+    while(num!=1) {
+        num = calc_squares_sum(digits_of_num(num));
+        if(num==1) {
+            return "happy";
+        }
+        if(num==tmp || counter>1000) {
+            return "sad";
+        }
+        counter++;
+    }  
+}
+
+
+int main() 
+{
+    cout << "32 is ... " << is_happy(32) << endl;
+    cout << "7 is ... " << is_happy(7) << endl;
+    cout << "28 is ... " << is_happy(28) << endl;
+    cout << "30 is ... " << is_happy(30) << endl;
+    cout << "9 is ... " << is_happy(9) << endl;
+    cout << "24 is ... " << is_happy(24) << endl;
+    cout << "13 is ... " << is_happy(13) << endl;
+    cout << "16 is ... " << is_happy(16) << endl;
+
+    return 0;
+}
